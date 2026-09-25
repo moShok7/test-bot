79944e16bd543dbc2c7299496a7f3ee7f36109f5
Шохджахон Тохиров <stohirov46@gmail.com>
2026-09-25 20:35:25 +0300

    Aff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            \App\Services\Telegram\UserTelegramService::class,
+            function ($app) {
+                return new \App\Services\Telegram\UserTelegramService();
+            }
+        );
     }
 
     /**
